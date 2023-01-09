83dac04522cc3579f929d725e6be32fe82ef00eb
Unnati Gupta <unnatixlr8@gmail.com>
2023-01-09 18:21:12 +0530

    Added Search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get 'home/index'
   root to: "home#index"
   get 'home/search'
+  get '/search', to: 'home#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
